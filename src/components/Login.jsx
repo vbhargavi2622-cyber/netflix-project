85abe7118806cd8c1864cd91b91b3e186fcb7247
Bhargavi Vajja <v.bhargavi525@gmail.com>
2025-10-24 13:38:52 +0530

    netflix signin and sign up form creation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+    const [isSignIn, setIsSignIn] = useState(true);
+    const toggleSignInForm = ()=>{
+        setIsSignIn(!isSignIn);
+    }
     return(
         <div className="Login-section">
             <Header/>
+            <div className="login-form">
+                <form>
+                    <h1>{isSignIn ? "Sign In" : "Sign Up"}</h1>
+                    {!isSignIn ? <input type="text" placeholder="Full Name" /> :""}
+                    <input type="text" placeholder="Email or Mobile Number" />
+                    <input type="password" placeholder="Password" />
+                    <button type="button" className="submit">{!isSignIn ? "Sign Up" : "Sign In" }</button>
+                    <span>{isSignIn ? "New to Netflix?" : "Already existing User Please." }</span>
+                    <button type="button" onClick={toggleSignInForm}>{isSignIn ? "Sign up now." : "Sign In Now" }</button>
+                </form>
+            </div>
         </div>
     )
 }
